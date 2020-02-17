5d251f49108b224b3741bf58a1362c341fa16362
zhangsan <zhangsan@itcast.com>
2020-02-17 22:11:16 +0800

    用idea把文件提交到git

---
+++ pushDemo/src/com/itcast/demo/pushDemo.java
@@ -0,0 +1,14 @@
+package com.itcast.demo;
+
+/**
+ * @Author: wuzhouwei
+ * @Date: 2020/2/17 21:47
+ */
+public class pushDemo {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
